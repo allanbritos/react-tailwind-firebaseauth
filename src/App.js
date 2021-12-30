0d4411dba62a1b242f44212246e69eaea130d28e
allan <allan@pop-os.localdomain>
2021-12-30 08:27:52 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+//import firebase from 'firebase/compat/app'
+//import 'firebase/compat/auth'
+
+import {
+    getAuth,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup,
+} from 'firebase/auth';
+
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [auth, setAuth] = useState(
+        false || window.localStorage.getItem('auth') === 'true'
+    );
+    const [token, setToken] = useState('');
+
+    const provider = new GoogleAuthProvider();
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuth(true);
+                window.localStorage.setItem('auth', 'true');
+                user.getIdToken().then((token) => {
+                    setToken(token);
+                });
+            } else {
+                // User is signed out
+                // ...
+            }
+        });
+    }, []);
+
+    const loginWithGoogle = () => {
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential =
+                    GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+            })
+            .catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.email;
+                // The AuthCredential type that was used.
+                const credential =
+                    GoogleAuthProvider.credentialFromError(error);
+            });
+    };
+
+    return (
+        <div className="App">
+            {auth ? (
+                <h1>Logged In</h1>
+            ) : (
+                <button onClick={loginWithGoogle}>Login with Google</button>
+            )}
+        </div>
+    );
 }
 
 export default App;
